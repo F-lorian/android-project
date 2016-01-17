06dedf410728ff3cb7d10c8747204f7f7122013f
Axel347 <axelgohan34@gmail.com>
2016-01-18 00:28:05 +0100

    Fix Error String + creation des classes de services

---
+++ android_client/SignProjectClient/app/src/main/java/services/ServiceNotificationSignalement.java
@@ -0,0 +1,7 @@
+package services;
+
+/**
+ * Created by Axel_2 on 18/01/2016.
+ */
+public class ServiceNotificationSignalement {
+}
--- android_client/SignProjectClient/app/src/main/java/services/ServiceSynchronisationGroupesUtilisateurs.java
+++
@@ -1,7 +0,0 @@
-package services;
-
-/**
- * Created by Axel_2 on 29/12/2015.
- */
-public class ServiceSynchronisationGroupesUtilisateurs {
-}
